0ed3cb90c81a4e7be34dc1972f1f9e342ac9e357
Eui Hyun Kim <hyomithetech@gmail.com>
2019-11-05 14:12:26 -0800

    Complete turing app into es6 class

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // TODO
+var GroceryListItem = ["Cereal", "kale", "oat meal"];
+
+var GroceryList = props => {
+  var onListItemClick = event => {
+    alert("Nice choice!");
+  };
+
+  return (
+    <ul>
+      {props.lists.map(list => (
+        <li onClick={onListItemClick}>{list}</li>
+      ))}
+    </ul>
+  );
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>My Grocery List</h2>
+        <GroceryList lists={GroceryListItem} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
