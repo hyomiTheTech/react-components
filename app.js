79c06a6313949b9804aeda424e5a9e4bbc13705d
Eui Hyun Kim <hyomithetech@gmail.com>
2019-11-05 22:21:06 -0800

    Change to es6 class

--- app.js
+++ app.js
@@ -2,14 +2,10 @@
 var GroceryListItem = ["Cereal", "kale", "oat meal"];
 
 var GroceryList = props => {
-  var onListItemClick = event => {
-    alert("Nice choice!");
-  };
-
   return (
     <ul>
       {props.lists.map(list => (
-        <li onClick={onListItemClick}>{list}</li>
+        <li>{list}</li>
       ))}
     </ul>
   );
@@ -18,13 +14,31 @@ var GroceryList = props => {
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
   }
 
   render() {
+    var style = {
+      textDecoration: this.state.done ? "line-through" : "none"
+    };
+
     return (
       <div>
         <h2>My Grocery List</h2>
-        <GroceryList lists={GroceryListItem} />
+        <GroceryList
+          style={style}
+          onClick={this.onListItemClick.bind(this)}
+          lists={GroceryListItem}
+        />
       </div>
     );
   }
